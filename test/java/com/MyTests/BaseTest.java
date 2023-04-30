d5da555d6374ce7f5c0e17e8662a0e76c963bd4f
Abhishek Deshmukh <abhishek.deshmukh@nagarro.com>
2023-04-30 20:43:48 +0530

    30 April 2023 AWS integration with selenium

--- test/java/com/MyTests/BaseTest.java
+++ test/java/com/MyTests/BaseTest.java
@@ -4,6 +4,7 @@
 package com.MyTests;
 
 import java.io.File;
+import java.net.URL;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,6 +12,7 @@ import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
@@ -32,17 +34,35 @@ public class BaseTest {
 	@BeforeMethod
 	@Parameters(value = { "browser" })
 	public void setUp(String browser) {
-		if (browser.equals("Chrome")) {
+		if (browser.equals("chrome")) {
 			WebDriverManager.chromedriver().setup();
-			driver = new ChromeDriver();
-		} else if (browser.equals("Firefox")) {
+//			driver = new ChromeDriver();
+			DesiredCapabilities cap = new DesiredCapabilities();
+			cap.setCapability("browserName", "chrome");
+			try {
+				driver = new RemoteWebDriver(new URL("http://65.0.32.160:4444/wd/hub"),cap);
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+		} else if (browser.equals("firefox")) {
 			WebDriverManager.firefoxdriver().setup();
-			File pathBinary = new File("C:\\Program Files\\Mozilla Firefox\\firefox.exe");
-			FirefoxBinary firefoxBinary = new FirefoxBinary(pathBinary);   
-			DesiredCapabilities desired = DesiredCapabilities.firefox();
-			FirefoxOptions options = new FirefoxOptions();
-			desired.setCapability(FirefoxOptions.FIREFOX_OPTIONS, options.setBinary(firefoxBinary));
-			driver = new FirefoxDriver(options);
+//			File pathBinary = new File("C:\\Program Files\\Mozilla Firefox\\firefox.exe");
+//			FirefoxBinary firefoxBinary = new FirefoxBinary(pathBinary);
+//			DesiredCapabilities desired = DesiredCapabilities.firefox();
+//			FirefoxOptions options = new FirefoxOptions();
+//			desired.setCapability(FirefoxOptions.FIREFOX_OPTIONS, options.setBinary(firefoxBinary));
+//			driver = new FirefoxDriver(options);
+			DesiredCapabilities cap = new DesiredCapabilities();
+			cap.setCapability("browserName", "firefox");
+			try {
+				driver = new RemoteWebDriver(new URL("http://65.0.32.160:4444/wd/hub"),cap);
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
 		} else {
 			System.out.println("no browser is defined in xml file ... ");
 		}
@@ -53,11 +73,11 @@ public class BaseTest {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		page = new BasePage(driver);  // this is very IMP to spread driver to all page classes 
+		page = new BasePage(driver); // this is very IMP to spread driver to all page classes
 	}
 
 	@AfterMethod
 	public void tearDown() {
-			driver.quit(); 
+		driver.quit();
 	}
 }
